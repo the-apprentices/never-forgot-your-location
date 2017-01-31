8ef84780f995ff20748d67676e8e813fe4442165
Nitin Dolli <nitin.dolli@gmail.com>
2017-01-31 23:21:58 +0800

    home screen design done

--- app/containers/home.js
+++ app/containers/home.js
@@ -9,12 +9,18 @@ export default class Home extends Component {
   render(){
     return (
       <View style={styles.container}>
-        <View style={styles.logo}>
+        <View style={[styles.logo, styles.col1]}>
           <Logo/>
         </View>
-        <FbLoginButton />
-        <Button buttonStyle={saveLocationButton} title='SAVE LOCATION' />
-        <Button buttonStyle={visitedPlacesButton} title='VISITED PLACES' />
+        <View style={[styles.social, styles.margin20, styles.col2]}>
+          <FbLoginButton />
+        </View>
+        <View style={[styles.button, styles.margin15, styles.col3]}>
+          <Button buttonStyle={saveLocationButton} title='SAVE LOCATION' />
+        </View>
+        <View style={[styles.button, styles.margin15, styles.col4]}>
+          <Button buttonStyle={visitedPlacesButton} title='VISITED PLACES' />
+        </View>
       </View>
     )
   }
@@ -43,8 +49,50 @@ const styles = StyleSheet.create({
     backgroundColor: '#F1F5F6',
     flex: 1,
     flexDirection: 'column',
+    justifyContent: 'flex-start'
   },
-  logo:{
+  logo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  social: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    flex: 1
+  },
+  button: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  margin20: {
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    marginLeft: 20,
+    marginRight: 20
+  },
+  margin15: {
+    marginLeft: 8,
+    marginRight: 8,
+  },
+  col1: {
     flex: 0.8,
+    top: 10
+  },
+  col2: {
+    flex: 1.2,
+    // marginTop:
+  },
+  col3: {
+    flex: 0.2,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  col4: {
+    flex: 0.45,
+    alignItems: 'stretch',
+    justifyContent: 'center',
   }
 });
